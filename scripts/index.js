015c06e0df8c2950309a4ef6ea6885127b2b41c3
Ivan Kashcheev <basiliris69@gmail.com>
2024-12-26 16:34:31 +0300

    added render create and delete functions in project

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const createCard = (card, deleteCard) => {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
+  return cardElement;
+};
+
+const deleteCard = (cardElement) => {
+  cardElement.remove();
+};
+
+const renderCards = (cards) => {
+  const placesContainer = document.querySelector(".places__list");
+  cards.forEach((card) => {
+    placesContainer.append(createCard(card, deleteCard));
+  });
+};
+
+renderCards(initialCards);
